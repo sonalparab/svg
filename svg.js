c6574ddfd83417c427591a502a52528b8302862e
sonalparab <sparab@stuy.edu>
2018-02-28 18:08:33 -0500

    finish

--- svg.js
+++ svg.js
@@ -5,11 +5,12 @@ var prevX;
 var prevY;
 var drawLine = 0;
 
-var addCircle = function(e){
+var draw = function(e){
 
     var xcor = e.offsetX;
     var ycor = e.offsetY;
 
+    //connect the circles if it is not the first cirle
     if(drawLine != 0){
 	var line = document.createElementNS('http://www.w3.org/2000/svg','line');
 	line.setAttribute('x1',prevX);
@@ -19,16 +20,17 @@ var addCircle = function(e){
 	line.setAttribute('stroke', 'black');
 	svg.appendChild(line);
     }
+
+    //draw the circle
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    //Once created, you can modify it with setAttribute calls
     c.setAttribute('cx',xcor);
     c.setAttribute('cy',ycor);
     c.setAttribute('r','20');
     c.setAttribute('fill','red');
     c.setAttribute('stroke','black');
-    c.setAttribute('stroke-width','2');
-    //To add the element to your svg container, use the appendChild function
     svg.appendChild(c);
+
+    //store the coordinates of the previous circle for connecting
     prevX = xcor;
     prevY = ycor;
     drawLine = 1;
@@ -39,5 +41,5 @@ var clearing = function(){
     drawLine = 0;
 }
 
-svg.addEventListener('click',addCircle);
+svg.addEventListener('click',draw);
 clear.addEventListener('click',clearing);
